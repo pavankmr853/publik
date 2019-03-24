7e06559f955cdbce705a0882d3ae05539229ef34
pavankmr853 <pavankmr853@gmail.com>
2019-03-24 22:00:30 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+// Require Sass file so webpack can build it
+import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import style from './styles/style.css';
+
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render((
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+ ), document.getElementById('root'));
